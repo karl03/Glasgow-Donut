34728ba3be7db00cf7f4a29fc308b41fe4741615
Michael McVey <2558835m@student.gla.ac.uk>
2022-11-23 13:50:11 +0000

    added google fonts to files

--- react-app/src/components/Admin/index.js
+++ react-app/src/components/Admin/index.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import { AdminAddData, AdminContainer, AdminDataListing, AdminDonutGraph } from './AdminElements'
 
+<head>
+  <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet"></link>
+</head>
+
 const Admin = () => {
   return (
     <AdminContainer>
         <AdminDataListing> 
-            <h1>List Data here</h1>
+          <span className="font-link">
+              <h1>List Data here</h1>
+          </span>
         </AdminDataListing>
         <AdminDonutGraph>
             <h1>Display Donut graph preview here</h1>
