6a1bc75834be2c7cba98c30b0615743015e66724
Permille <9145570@gmail.com>
2022-12-01 03:11:37 +0000

    Improved styling for groups

--- react-app/src/components/Admin/AdminSliderGroup.js
+++ react-app/src/components/Admin/AdminSliderGroup.js
@@ -1,7 +1,22 @@
 import AdminSlider from "./AdminSlider";
+import styled from "styled-components";
+const AdminSliderWrapper = styled.div`
+  width: calc(100% - 20px);
+  align-items: center;
+  padding: 10px;
+  margin: 0 10px 10px 10px;
+  box-sizing: border-box;
+  background-color: #eeeeee;
+`;
+const Title = styled.h2`
+  text-align: center;
+  margin: 10px;
+`;
+
 export default function AdminSliderGroup({sliders, eventHandler, groupID}){
   return (
-    <div>
+    <AdminSliderWrapper>
+      <Title>Group {groupID}</Title>
       {
         sliders.map((slider) => (
           <AdminSlider
@@ -14,6 +29,6 @@ export default function AdminSliderGroup({sliders, eventHandler, groupID}){
           />
         ))
       }
-    </div>
+    </AdminSliderWrapper>
   );
 };
