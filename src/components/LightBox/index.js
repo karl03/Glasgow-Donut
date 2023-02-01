b5d67ba7ee4abca46487f544673de763947cd3bb
2551634s <2551634s@student.gla.ac.uk>
2023-02-01 14:07:44 +0000

    names display inside lightbox and css changes

--- src/components/LightBox/index.js
+++ src/components/LightBox/index.js
@@ -3,8 +3,12 @@ import "./Lightbox.css";
 
 
 export default function LightBox ({trigger, setTrigger, DataProperty, EventProperty}){
+  
+  const [Name,setName] = React.useState(DataProperty[0]);
+
   useEffect(() => {
     if(trigger==="active"){
+      setName(DataProperty[0].split('_').join(' '));
       setTrigger("active")
     }
   });
@@ -20,9 +24,11 @@ export default function LightBox ({trigger, setTrigger, DataProperty, EventPrope
   return (
   <div className={trigger} onClick={changeState}>
     <span id="circle">
-      <img id="lightbox_img" src={EventProperty?.target?.href?.baseVal ?? 4} alt={DataProperty.Name}/>
-      <h1 id="lightbox_title">{DataProperty.Name}</h1>
-      </span>
+      <div id="lightbox_wrapper">
+        <img id="lightbox_img" src={EventProperty?.target?.href?.baseVal ?? 4} alt={DataProperty.Name}/>
+        <h1 id="lightbox_title">{Name}</h1>
+      </div>
+    </span>
   </div>
     );
   };
