fccc77561c723c4b53aaea13ebaf4de7f86b45ad
M Wraith-Whiting <2560800w@student.gla.ac.uk>
2023-02-01 16:30:55 +0000

    added Array prop to allow dynamic contents - passing an array of options to the component now works

--- src/components/InterfaceComponents/DropDown.js
+++ src/components/InterfaceComponents/DropDown.js
@@ -1,28 +1,23 @@
 import React, { useState } from 'react'
 import './DropDown.css'
 
-export default function DropDown() {
-    const [isOpen, setOpen] = useState(false);
+export default function DropDown({ dataArray } ) {
+  const [isOpen, setOpen] = useState(false);
 
-    const x =  ['a', 'b', 'c'];
 
-    function handleOpen(){
-        setOpen(!isOpen);
-    }
+  function handleOpen(){
+      setOpen(!isOpen);
+  }
 
   return (
     <div className='dropdown'>
       <button onClick={handleOpen}> {"" + isOpen}</button>
       {isOpen ? 
       <ul className='menu'>
-        {/* <li> <button className='menu-item'> Button 1</button></li>
-        <li> <button className='menu-item'> Button 2</button></li>
-        <li> <button className='menu-item'> Button 3</button></li> */}
-        {x.map((xItem, index) => (
-            <li key={index} className="menu-item">{xItem}</li>
+        {dataArray.map((Item, index) => (
+            <li key={index} className="menu-item">{Item}</li>
         ))}
       </ul> : null}
-      {isOpen ? <div>Is open!</div> : <div>Is closed!</div>}
     </div>
   )
 }
