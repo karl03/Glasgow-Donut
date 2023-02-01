f626878f3083434090aba610f52df424c2d471b1
M Wraith-Whiting <2560800w@student.gla.ac.uk>
2023-02-01 14:51:07 +0000

    Implemented dynamic dropdown list with test list

--- src/components/InterfaceComponents/DropDown.js
+++ src/components/InterfaceComponents/DropDown.js
@@ -4,6 +4,8 @@ import './DropDown.css'
 export default function DropDown() {
     const [isOpen, setOpen] = useState(false);
 
+    const x =  ['a', 'b', 'c'];
+
     function handleOpen(){
         setOpen(!isOpen);
     }
@@ -13,9 +15,12 @@ export default function DropDown() {
       <button onClick={handleOpen}> {"" + isOpen}</button>
       {isOpen ? 
       <ul className='menu'>
-        <li> <button className='menu-item'> Button 1</button></li>
+        {/* <li> <button className='menu-item'> Button 1</button></li>
         <li> <button className='menu-item'> Button 2</button></li>
-        <li> <button className='menu-item'> Button 3</button></li>
+        <li> <button className='menu-item'> Button 3</button></li> */}
+        {x.map((xItem, index) => (
+            <li key={index} className="menu-item">{xItem}</li>
+        ))}
       </ul> : null}
       {isOpen ? <div>Is open!</div> : <div>Is closed!</div>}
     </div>
