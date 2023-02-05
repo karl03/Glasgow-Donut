141c7e79f5e386db0b5605ff8f8f7854e5983b81
2551634s <2551634s@student.gla.ac.uk>
2023-02-05 22:08:52 +0000

    names should now display inside lightbox also added additional circles js

--- src/components/LightBox/index.js
+++ src/components/LightBox/index.js
@@ -5,6 +5,8 @@ import "./Lightbox.css";
 export default function LightBox ({trigger, setTrigger, DataProperty, EventProperty}){
   
   const [Name,setName] = React.useState(DataProperty[0]);
+  const [Hide, setHidden] = React.useState("circle_hidden");
+  const [HideIndicator,setIndicator] = React.useState("hidden");
 
   useEffect(() => {
     if(trigger==="active"){
@@ -16,19 +18,39 @@ export default function LightBox ({trigger, setTrigger, DataProperty, EventPrope
   function changeState() {
     console.log("LightBox ChangeState function called!");
     if(trigger ==="active"){
+      setHidden("circle_hidden");
       setTrigger("hidden")
       document.body.id="show_scroll"
     }
   }
 
+  function additionalCircles() {
+    if(Hide=="circle_hidden"){
+      setHidden("circle_active");
+    }
+  }
+  
+  function showIndicator() {
+    if(HideIndicator == "hidden"){
+      setIndicator("shown");
+    }else{
+      setIndicator("hidden");
+    }
+  }
+
   return (
-  <div className={trigger} onClick={changeState}>
-    <span id="circle">
-      <div id="lightbox_wrapper">
-        <img id="lightbox_img" src={EventProperty?.target?.href?.baseVal ?? 4} alt={DataProperty.Name}/>
-        <h1 id="lightbox_title">{Name}</h1>
-      </div>
+    <>
+  <div className={trigger} id="lightbox" onClick={changeState}>
+  </div>
+  <div id="lightbox_wrapper">
+    <span className={"circle " + trigger} onClick={additionalCircles}>
+      <img id="lightbox_img" onClick={additionalCircles} src={EventProperty?.target?.href?.baseVal ?? 4} alt={DataProperty.Name}/>
+      <h1 id="lightbox_title" onClick={additionalCircles}>{Name}</h1>
+    </span>
+    <span className="circle" id={Hide} onClick={showIndicator}>
+      <h1 id="lightbox_title">{DataProperty[1]?.target ?? "why are you here go away"}</h1>
     </span>
   </div>
+  </>
     );
   };
