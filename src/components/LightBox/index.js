ba1a8e57900510b3c1d71818289a34f4dff45eb3
2551634s <2551634s@student.gla.ac.uk>
2023-02-06 00:45:03 +0000

    fixed syntax and removed unused function

--- src/components/LightBox/index.js
+++ src/components/LightBox/index.js
@@ -6,14 +6,13 @@ export default function LightBox ({trigger, setTrigger, DataProperty, EventPrope
   
   const [Name,setName] = React.useState(DataProperty[0]);
   const [Hide, setHidden] = React.useState("circle_hidden");
-  const [HideIndicator,setIndicator] = React.useState("hidden");
 
   useEffect(() => {
     if(trigger==="active"){
       setName(DataProperty[0].split('_').join(' '));
       setTrigger("active")
     }
-  });
+  },[trigger, DataProperty, setTrigger]);
 
   function changeState() {
     console.log("LightBox ChangeState function called!");
@@ -25,16 +24,8 @@ export default function LightBox ({trigger, setTrigger, DataProperty, EventPrope
   }
 
   function additionalCircles() {
-    if(Hide=="circle_hidden"){
+    if(Hide==="circle_hidden"){
       setHidden("circle_active");
-    }
-  }
-  
-  function showIndicator() {
-    if(HideIndicator == "hidden"){
-      setIndicator("shown");
-    }else{
-      setIndicator("hidden");
     }
   }
 
@@ -47,7 +38,7 @@ export default function LightBox ({trigger, setTrigger, DataProperty, EventPrope
       <img id="lightbox_img" onClick={additionalCircles} src={EventProperty?.target?.href?.baseVal ?? 4} alt={DataProperty.Name}/>
       <h1 id="lightbox_title" onClick={additionalCircles}>{Name}</h1>
     </span>
-    <span className="circle" id={Hide} onClick={showIndicator}>
+    <span className="circle" id={Hide}>
       <h1 id="lightbox_title">{DataProperty[1]?.target ?? "why are you here go away"}</h1>
     </span>
   </div>
