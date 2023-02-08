a100856291cd9c5123dfd1713d9c1177e4509f5f
M Wraith-Whiting <2560800w@student.gla.ac.uk>
2023-02-08 11:30:35 +0000

    added OnClick debug function to list elements - added dropdown to AdminPage with [1,2,3] - added function to print to console OnClick

--- src/components/InterfaceComponents/DropdownMenu.js
+++ src/components/InterfaceComponents/DropdownMenu.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
-import './DropDown.css'
+import './DropdownMenu.css'
 
-export default function DropDown({ dataArray } ) {
+export default function DropdownMenu({ dataArray } ) {
   if (dataArray === undefined) {
     dataArray = [];
   }
@@ -9,13 +9,11 @@ export default function DropDown({ dataArray } ) {
   const [isOpen, SetOpen] = useState(false);
 
   return (
-    <div className='dropdown'>
+    <div className='menu'>
       <button onClick={() => SetOpen(!isOpen)}> {"" + isOpen}</button>
       {isOpen ? 
       <ul className='menu'>
-        {dataArray.map((Item, index) => (
-            <li key={index} className="menu-item">{Item}</li>
-        ))}
+        {dataArray.map((Item, index) => (<li key={index} className="menu-item" onClick={function(){console.log(Item + " at " + index + " was clicked!");}}>{Item}</li> ))} 
       </ul> : null}
     </div>
   )
