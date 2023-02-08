d2c03a0fd6109479940ba077ed2ec8837fc056b4
M Wraith-Whiting <2560800w@student.gla.ac.uk>
2023-02-08 15:05:52 +0000

    created basic ModalMenu component and css file - ModalMenu is a div with 3 sub divs for header, body, footer

---
+++ src/components/InterfaceComponents/ModalMenu.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function ModalMenu() {
+  return (
+    <div className='modal'>
+        <div className="modal-content">
+
+            <div className="modal-header">
+                <h4 className="modal-title">Modal Menu</h4>
+            </div>
+
+            <div className="modal-body">
+                This is where the main content is.
+            </div>
+
+            <div className="modal-footer">
+                <button className="modal-button">Close</button>
+            </div>
+            
+        </div>
+    </div>
+  )
+}
