6a54c5229fc5e655edcc57b8fd3afe927549bdfe
M Wraith-Whiting <2560800w@student.gla.ac.uk>
2023-02-08 11:39:18 +0000

    Added a handleSelection function - A function with the provided arguments is passed via the OnClick Event

--- src/components/InterfaceComponents/DropdownMenu.js
+++ src/components/InterfaceComponents/DropdownMenu.js
@@ -8,12 +8,17 @@ export default function DropdownMenu({ dataArray } ) {
 
   const [isOpen, SetOpen] = useState(false);
 
+  function handleSelection(item, index){
+    console.log(item + " at " + index + " was clicked!");
+    SetOpen(!isOpen);
+  }
+
   return (
     <div className='menu'>
       <button onClick={() => SetOpen(!isOpen)}> {"" + isOpen}</button>
       {isOpen ? 
       <ul className='menu'>
-        {dataArray.map((Item, index) => (<li key={index} className="menu-item" onClick={function(){console.log(Item + " at " + index + " was clicked!");}}>{Item}</li> ))} 
+        {dataArray.map((item, index) => (<li key={index} className="menu-item" onClick={function(){handleSelection(item, index)}}>{item}</li> ))} 
       </ul> : null}
     </div>
   )
