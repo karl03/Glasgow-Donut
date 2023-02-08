87a51763a9c9888081cb978f82eb62535b17139a
M Wraith-Whiting <2560800w@student.gla.ac.uk>
2023-02-08 11:44:30 +0000

    Converted function generation to a single method - Replaced the anonymous function creation with attributes - data-item and data-index are now attributes / parameters

--- src/components/InterfaceComponents/DropdownMenu.js
+++ src/components/InterfaceComponents/DropdownMenu.js
@@ -8,7 +8,9 @@ export default function DropdownMenu({ dataArray } ) {
 
   const [isOpen, SetOpen] = useState(false);
 
-  function handleSelection(item, index){
+  const handleSelection = (e) =>{
+    const item = e.currentTarget.getAttribute("data-item");
+    const index = e.currentTarget.getAttribute("data-index");
     console.log(item + " at " + index + " was clicked!");
     SetOpen(!isOpen);
   }
@@ -18,7 +20,7 @@ export default function DropdownMenu({ dataArray } ) {
       <button onClick={() => SetOpen(!isOpen)}> {"" + isOpen}</button>
       {isOpen ? 
       <ul className='menu'>
-        {dataArray.map((item, index) => (<li key={index} className="menu-item" onClick={function(){handleSelection(item, index)}}>{item}</li> ))} 
+        {dataArray.map((item, index) => (<li key={index} className="menu-item" data-index={index} data-item={item} onClick={handleSelection}>{item}</li> ))} 
       </ul> : null}
     </div>
   )
