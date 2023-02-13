4c1fb60a16935aeee1f00021a8997028cf6cd03a
M Wraith-Whiting <2560800w@student.gla.ac.uk>
2023-02-13 11:40:30 +0000

    added basic form elements

--- src/components/InterfaceComponents/AddSectorModal.js
+++ src/components/InterfaceComponents/AddSectorModal.js
@@ -11,8 +11,32 @@ export default function AddSectorModal(props) {
         onSave={() => setShowingModal(false)} // TODO: onSave function to pass data from Modal
         title="Modal Title"
     >
-        <form action="">
-
+        <form action="" className="add-sector-form">
+            <div>
+                <label for="sector-title">Title: </label>
+                <input type="text" className="sector-title" />
+            </div>
+            <div>SLIDER INSERT</div>
+            <div>
+                <label for="sector-indicator">Indicator: </label>
+                <input type="text" className="sector-indicator" />
+            </div>
+            <div>
+                <label for="sector-target">target: </label>
+                <input type="text" className="sector-target" />
+            </div>
+            <div>
+                <label for="sector-description">description: </label>
+                <input type="text" className="sector-description" />
+            </div>
+            <div>
+                <label for="sector-cites">Citations: </label>
+                <input type="text" className="sector-cites" />
+            </div>
+            <div>
+                <label for="sector-videolink">Videolink: </label>
+                <input type="url"  className="sector-videolink" />
+            </div>
         </form>
     </ModalMenu>
   )
