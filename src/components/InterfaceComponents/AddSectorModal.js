9e09b9f3a267934a95992571f5384e53c6acbcdb
M Wraith-Whiting <2560800w@student.gla.ac.uk>
2023-02-13 11:35:28 +0000

    Added initial setup for ModalMenu

--- src/components/InterfaceComponents/AddSectorModal.js
+++ src/components/InterfaceComponents/AddSectorModal.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import ModalMenu from './ModalMenu'
 
-export default function AddSectorModal() {
+export default function AddSectorModal(props) {
+    const [isShowingModal, setShowingModal] = useState(false);
+
   return (
-    <ModalMenu>
-        
+    <ModalMenu 
+        isShow={isShowingModal}
+        onClose={() => setShowingModal(false)}
+        onSave={() => setShowingModal(false)} // TODO: onSave function to pass data from Modal
+        title="Modal Title"
+    >
+        <form action="">
+
+        </form>
     </ModalMenu>
   )
 }
