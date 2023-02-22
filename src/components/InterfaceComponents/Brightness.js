2008e2691a349cad001fe16f6861e06cc20a1869
Michael McVey <2558835m@student.gla.ac.uk>
2023-02-22 11:47:53 +0000

    Brightness slider mostly working, just needs formatting

--- src/components/InterfaceComponents/Brightness.js
+++ src/components/InterfaceComponents/Brightness.js
@@ -8,14 +8,15 @@ export default function Brightness() {
     function handleSlider(e, setBrightness)
     {
     var container = document.getElementById('container');
-    setBrightness(e.value);
-    container.setAttribute("style", "filter: brightness("+brightness+"%);");
+    setBrightness(e.target.value);
+    console.log(e);
+    //document.body.style.filter = "brightness("+brightness+"%)";
 }
   return (
 
-    <div id="container">
+    <div style={{top: "0", left: "0", width: "100vw", height: "100vh", zIndex: "1000", position: "fixed", backgroundColor:"rgba(0, 0, 0, " + (brightness / 100) + ")"}}>
     <div class="main">
-        <input type="range" id="brightness-range" min="10" max="100" onchange={(e) => handleSlider(e, setBrightness)}>
+        <input type="range" id="brightness-range" min="10" max="100" onInput={(e) => {handleSlider(e, setBrightness);}}>
         </input>
     </div>
     </div>  
