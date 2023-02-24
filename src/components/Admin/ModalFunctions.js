4138bff1389561ef76dd9b66d1d3f1865ea0c239
M Wraith-Whiting <2560800w@student.gla.ac.uk>
2023-02-24 11:55:59 +0000

    migrated AddSectorModal functions

--- src/components/Admin/ModalFunctions.js
+++ src/components/Admin/ModalFunctions.js
@@ -1,7 +1,7 @@
 // Functions for the modal menu!
 
 // get all form elements
-export function getFormElements(){
+function getFormElements(){
     const title = document.getElementById('modal-sector-title');
     const value = document.getElementById('modal-sector-value');
     const indicator = document.getElementById('modal-sector-indicator');
@@ -13,30 +13,90 @@ export function getFormElements(){
     return [title, value, indicator, target, description, cites, videolink];
 }
 
-export function populateForm(sliderGroups, formElements, name, ecoOrSoc, gloOrLoc){
+export function populateForm(sliderGroups, name, ecoOrSoc, gloOrLoc){
     // Find existing data
 
-    const data = sliderGroups;
+    const data = sliderGroups[ecoOrSoc][gloOrLoc][name];
 
-    console.log(name, JSON.stringify(data));
+    console.log("populateForm: ", name, JSON.stringify(data));
 
 
     // write data to elemenets
+    const [title, value, indicator, target, description, cites, videolink] = getFormElements();;
+    title.value = name;
+    value.value = data['value'];
+    indicator.value = data['indicator'];
+    target.value = data['target'];
+    description.value = data['description'];
+    cites.value = data['quotes'];
+    videolink.value = data['video_hash'];
 
 }
 
-export function onClose(){
-    // on close
-        // clear form
+export function onClose(setShow){
+    const formElements = getFormElements();
+    // Clear form.
+    formElements.forEach(x => {
+        x.value = '';
+    });
 
-        // close modal
+    // Close modal.
+    setShow(false);
 }
 
-export function onSave(){
-    // on save
-        // delete exisiting slider from data.json
-        // insert form data to replace old slider
-        // clear form
-        // close modal
+function isValidForm(title){
+    if (title !== '') {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
 
+function getFormData(formElements){
+    let formData = {};
+    for (var key in formElements){
+        const value = formElements[key].value;
+        if (typeof(value) === "undefined") {
+            formData[key] = '';
+        }
+        else{
+            formData[key] = value;
+        }
+    }
+
+    return formData;
+}
+
+export function onSave(sliderGroups, setSliderGroups, ecoOrSoc, gloOrLoc, setShow){
+    // get form data
+    const {title, value, indicator, target, description, cites, videolink} = getFormData(getFormElements());
+
+    if (!isValidForm(title.value)) {
+        alert("The sector requires a title!");
+        return;
+    }
+
+    console.log("handleSubmit: " + title, 
+    value, 
+    indicator, 
+    target, 
+    description, 
+    cites, 
+    videolink)
+    // insert/overwrite new data 
+
+    const New = JSON.parse(JSON.stringify(sliderGroups));
+    New[ecoOrSoc][gloOrLoc][title] = {
+        "value": value,
+        "adjacent": [],//NEEDS TO BE IMPLEMENTED
+        "indicator": indicator,
+        "target": target,
+        "description": description,
+        "quotes": cites,
+        "symbol_id": '', //NEEDS TO BE IMPLEMENTED
+        "video_hash": videolink
+    };
+    setSliderGroups(New);
+    onClose(setShow)
 }
