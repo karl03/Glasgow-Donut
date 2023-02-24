4e9c6fb17f91b829812a3824787a2754b46a6fdd
M Wraith-Whiting <2560800w@student.gla.ac.uk>
2023-02-24 23:48:32 +0000

    updated DropdownMenu component

--- src/components/InterfaceComponents/DropdownMenu.js
+++ src/components/InterfaceComponents/DropdownMenu.js
@@ -7,17 +7,20 @@ export default function DropdownMenu({ dataArray } ) {
   }
 
   const [isOpen, SetOpen] = useState(false);
+  const [selection, SetSelection] = useState('');
 
   const handleSelection = (e) =>{
     const item = e.currentTarget.getAttribute("data-item");
     const index = e.currentTarget.getAttribute("data-index");
     console.log(item + " at " + index + " was clicked!");
+    SetSelection(item);
     SetOpen(!isOpen);
   }
 
   return (
     <div className='menu'>
-      <button onClick={() => SetOpen(!isOpen)}> {"" + isOpen}</button>
+      <button className='dropdown_button' onClick={() => SetOpen(!isOpen)}> {selection === '' ? 'Select...' : selection}</button>
+      <button className='dropdown_deselect_button' onClick={() => SetSelection('')}>X</button>
       {isOpen ? 
       <ul className='menu'>
         {dataArray.map((item, index) => (<li key={index} className="menu-item" data-index={index} data-item={item} onClick={handleSelection}>{item}</li> ))} 
