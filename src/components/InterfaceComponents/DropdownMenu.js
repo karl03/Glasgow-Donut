d5ccd0b8caa29c93bad57fbf17942d7666d345dc
M Wraith-Whiting <2560800w@student.gla.ac.uk>
2023-02-27 12:34:08 +0000

    added notifyFunction and onClear props - allow function calls on selection (notifyFunction) - onClear prop allows function call on clear selection

--- src/components/InterfaceComponents/DropdownMenu.js
+++ src/components/InterfaceComponents/DropdownMenu.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react'
 import './DropdownMenu.css'
 
-export default function DropdownMenu({ dataArray } ) {
-  if (dataArray === undefined) {
-    dataArray = [];
-  }
+export default function DropdownMenu(props) {
+  const dataArray = props.dataArray === undefined ? [] : props.dataArray;
+  if (props.notifyFunction === undefined) props.notifyFunction = function() {return undefined;};
 
   const [isOpen, SetOpen] = useState(false);
   const [selection, SetSelection] = useState('');
@@ -12,15 +11,30 @@ export default function DropdownMenu({ dataArray } ) {
   const handleSelection = (e) =>{
     const item = e.currentTarget.getAttribute("data-item");
     const index = e.currentTarget.getAttribute("data-index");
-    console.log(item + " at " + index + " was clicked!");
-    SetSelection(item);
-    SetOpen(!isOpen);
+
+    try {
+      props.notifyFunction(item);
+      SetSelection(item);
+      SetOpen(!isOpen);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  function handleOnClear(){
+    try {
+      props.onClear();
+      SetSelection('');
+      props.notifyFunction('');
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
     <div className='menu'>
       <button className='dropdown_button' onClick={() => SetOpen(!isOpen)}> {selection === '' ? 'Select...' : selection}</button>
-      <button className='dropdown_deselect_button' onClick={() => SetSelection('')}>X</button>
+      <button className='dropdown_deselect_button' onClick={handleOnClear}>X</button>
       {isOpen ? 
       <ul className='menu'>
         {dataArray.map((item, index) => (<li key={index} className="menu-item" data-index={index} data-item={item} onClick={handleSelection}>{item}</li> ))} 
