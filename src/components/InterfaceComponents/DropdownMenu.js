a71bca996d985d8485308d33a5ea5da4f900a29e
M Wraith-Whiting <2560800w@student.gla.ac.uk>
2023-02-28 14:59:26 +0000

    added disabled functionality - can no have a disable DropdownMenu

--- src/components/InterfaceComponents/DropdownMenu.js
+++ src/components/InterfaceComponents/DropdownMenu.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './DropdownMenu.css'
 
 export default function DropdownMenu(props) {
@@ -7,6 +7,7 @@ export default function DropdownMenu(props) {
 
   const [isOpen, SetOpen] = useState(false);
   const [selection, SetSelection] = useState('');
+  const [disabled, SetDisabled] = useState(true);
 
   const handleSelection = (e) =>{
     const item = e.currentTarget.getAttribute("data-item");
@@ -31,14 +32,26 @@ export default function DropdownMenu(props) {
     }
   }
 
+  useEffect(() => {
+    const dis = props.disabled || false;
+    SetDisabled(dis)
+  }, [props])
+
   return (
     <div className='menu'>
-      <button className='dropdown_button' onClick={() => SetOpen(!isOpen)}> {selection === '' ? 'Select...' : selection}</button>
-      <button className='dropdown_deselect_button' onClick={handleOnClear}>X</button>
+      <button className='dropdown_button' disabled={disabled} onClick={() => SetOpen(!isOpen)}> {selection === '' ? 'Select...' : selection}</button>
+      <button className='dropdown_deselect_button' disabled={disabled} onClick={handleOnClear}>X</button>
       {isOpen ? 
       <ul className='menu'>
         {console.log(dataArray)}
-        {dataArray.map((item, index) => (<li key={index} className="menu-item" data-index={index} data-item={item} onClick={handleSelection}>{item}</li> ))} 
+        {dataArray.map((item, index) => (
+          <li 
+            key={index} 
+            className="menu-item" 
+            data-index={index} 
+            data-item={item} 
+            onClick={handleSelection}
+          >{item}</li> ))} 
       </ul> : null}
     </div>
   )
